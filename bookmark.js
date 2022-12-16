b87e9bf46c2d27499e664c19f270f03bf5bea6b3
govind <govindhake39@gmail.com>
2022-12-16 22:39:46 +0530

    day-4 branch added

--- bookmark.js
+++ bookmark.js
@@ -1,5 +1,34 @@
-// append data in div using javascript?
 
 
+async function fetchBooking(){
+    
+    try {
+        let fetchHotels= await fetch("https://639ad40131877e43d677b046.mockapi.io/hotels",{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization : `Bearer ${sessionStorage.getItem("")}`
+            }
+        });
 
+        if(fetchHotels.ok===true){
+            alert("successfully fetched")
+            let fetchData= await fetchHotels.json();
+            renderHotels(fetchData);
+        }
+    } catch (error) {
+        alert("Bad Request")
+    }
+}
 
+fetchBooking();
+
+// function renderHotels(data){
+//     let main_container= document.getElementById("main-container");
+//     main_container.innerHTML="";
+//     data.forEach((el)=>{
+       
+
+//     })
+
+// }
